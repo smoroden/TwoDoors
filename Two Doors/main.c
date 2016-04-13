a2cc5a3802828f39c9015a4368182231cc592f33
Zach Smoroden <zach@smoroden.com>
2016-04-13 14:50:04 -0700

    Initial Commit

---
+++ Two Doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Two Doors
+//
+//  Created by Zach Smoroden on 2016-04-13.
+//  Copyright © 2016 Zach Smoroden. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
